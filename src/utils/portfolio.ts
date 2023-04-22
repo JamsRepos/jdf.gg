10b716f87ed143811955fb957e523aeea3506075
James Finch <LubricantJam@users.noreply.github.com>
2023-04-22 01:49:10 +0100

    Removed the list of current technologies.

--- src/utils/portfolio.ts
+++ src/utils/portfolio.ts
@@ -97,17 +97,17 @@ export const aboutSection: AboutSectionType = {
   title: 'About me',
   // Paragraphs need to be changed from containers/About.tsx
   // Because it wasn't possible to insert anchor tags like this
-  list: {
-    title: 'Here are a few technologies I’ve been working with recently:',
-    items: [
-      'Docker',
-      'Zero Trust',
-      'Linux CLI',
-      'Craft CMS',
-      'Tailwind CSS',
-      'NPM',
-    ],
-  },
+  // list: {
+  //   title: 'Here are a few technologies I’ve been working with recently:',
+  //   items: [
+  //     'Docker',
+  //     'Zero Trust',
+  //     'Linux CLI',
+  //     'Craft CMS',
+  //     'Tailwind CSS',
+  //     'NPM',
+  //   ],
+  // },
   img: '/images/greyscale.jpg',
 };
 
