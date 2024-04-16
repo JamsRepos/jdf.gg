fa52cfdabbe3b05d427bd776182da04c8c8d95b3
Jam <1347620+JamsRepos@users.noreply.github.com>
2024-04-16 23:58:56 +0100

    feat: :sparkles: Added torch effect to cursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
+import { Cursor } from '../components';
 import { ThemeProvider } from '../context/theme-context';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Cursor className="hidden dark:lg:block" />
       <Component {...pageProps} />
       <Analytics />
     </ThemeProvider>
